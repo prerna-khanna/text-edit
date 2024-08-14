eb0e3fc46f078b1e76a061b45013a6f8c36065dc
Prerna Khanna <pkhanna@cs.stonybrook.edu>
2024-08-14 00:37:44 -0400

    added watch IMU

---
+++ gesture Watch App/gestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  gestureApp.swift
+//  gesture Watch App
+//
+//  Created by Prerna Khanna on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct gesture_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
